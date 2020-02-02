6de242fadc1e08257a9c82f7567d5867473c88c8
Haley Kelling <haley@kellings.com>
2020-02-02 16:28:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(board, index) == true
-      move(board, input_to_index(input))
+      move(board, index)
       display_board(board)
       break
     end
